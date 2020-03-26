fe823efa8254bc810502d7585c4357db282c1b17
BrunoMendes <bruno_f_4@hotmail.com>
2020-03-26 10:54:32 -0300

    Initial commit

---
+++ app/src/main/java/com/leaoartes/projeto_sorteio/MainActivity.java
@@ -0,0 +1,25 @@
+package com.leaoartes.projeto_sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view){
+        TextView text = findViewById(R.id.textView2);
+        int number = new Random().nextInt(11);
+        text.setText("Número: " + number);
+    }
+
+}
